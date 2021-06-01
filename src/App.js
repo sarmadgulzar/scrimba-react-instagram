eb27c8a0ab6ace50be7d3c58ef7419dadf2a380a
Sarmad Gulzar <sarmadgulzar@outlook.com>
2021-06-02 01:21:27 +0500

    Added routes (#3)

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+
+const Dashboard = lazy(() => import("./pages/dashboard"));
+const Login = lazy(() => import("./pages/login"));
+const Signup = lazy(() => import("./pages/signup"));
+const Profile = lazy(() => import("./pages/profile"));
+const NotFound = lazy(() => import("./pages/not-found"));
+
+export default function App() {
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={Signup} />
+          <Route path={ROUTES.PROFILE} component={Profile} />
+          <Route path={ROUTES.NOT_FOUND} component={NotFound} exact />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+}
---
+++ src/pages/not-found.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NotFound() {
+  return <p>Hello from NotFound</p>;
+}
